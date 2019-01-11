283e27b35594a36c00556a9e66bc4471cc5b65b3
avangardesoftwarelena2018 <avangardesoftwarelena2018@gmail.com>
2019-01-11 16:38:14 +0200

    Properties UI work in progress

--- Assets/Scripts/UI/PropertyUIManager.cs
+++ Assets/Scripts/UI/PropertyUIManager.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UITest : MonoBehaviour
+public class PropertyUIManager : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject propertyPrefabButton; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InstantiateProperties();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void InstantiateProperties()
+    {
+
     }
 }
