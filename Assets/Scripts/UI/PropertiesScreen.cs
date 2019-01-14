1f4be6a169cb858493da170590a1747f4f34df6d
avangardesoftwarelena2018 <avangardesoftwarelena2018@gmail.com>
2019-01-14 16:10:46 +0200

    Properties Screen

---
+++ Assets/Scripts/UI/PropertiesScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertiesScreen : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject propertyPrefabButton;
+    [SerializeField]
+    public Transform propertyInfoContent; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstantiateProperties();
+    }
+
+    public void AddPropertyItem()
+    {
+        PropertyData data = PropertyDataManager.GetPropertyData();
+        Property propertyItem = new Property();
+        data.properties.Add(propertyItem);
+        PropertyDataManager.SetPropertyData(data);
+    }
+
+    private void InstantiateProperties()
+    {
+        //if (PropertyDataManager.GetPropertyData().properties != null)
+        {
+            foreach (var property in PropertyDataManager.GetPropertyData().properties)
+            {
+                GameObject propertyButton = Instantiate(propertyPrefabButton, propertyInfoContent);
+                propertyButton.GetComponent<PropertyFields>().Initialize(property);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PropertyFields.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PropertyFields : MonoBehaviour
+{
+    [SerializeField]
+    private Text proprietyName;
+    [SerializeField]
+    private Text propertySingleBedsRoom;
+    [SerializeField]
+    private Text propertyDoubleBedsRoom;
+    
+    public void Initialize(Property property)
+    {
+        proprietyName.text = property.name;
+    }
+}
