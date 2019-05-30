01d9d5dd997654482300bc7d9fcce454527e9174
SergiuAvangarde <sergiu.pop@avangarde-software.com>
2019-05-30 17:40:49 +0300

    UI fixes, input field working properly, changed collor code, adjusted text size and font, adjusted main menu elements position and scale

--- Assets/Scripts/UI/InputManager.cs
+++ Assets/Scripts/UI/InputManager.cs
@@ -1,5 +1,7 @@
 using UINavigation;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
@@ -8,8 +10,28 @@ public class InputManager : MonoBehaviour
 
     [SerializeField]
     private Navigator navigator = null;
+    [SerializeField]
+    private Image quitObjectImage;
+    [SerializeField]
+    private Text quitObjectText;
 
-    void Update()
+    private bool clickedBefore = false;
+    private Color imageDefaultColor;
+    private Color textDefaultColor;
+    private Color currentImageCollor;
+    private Color currentTextCollor;
+
+    private void Start()
+    {
+        imageDefaultColor = quitObjectImage.color;
+        textDefaultColor = quitObjectText.color;
+        currentImageCollor.a = 0;
+        currentTextCollor.a = 0;
+        quitObjectImage.color = currentImageCollor;
+        quitObjectText.color = currentTextCollor;
+    }
+
+    private void Update()
     {
         // handle Android back key
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -21,9 +43,18 @@ public class InputManager : MonoBehaviour
             else
             {
                 bool shouldMinimizeApp = navigator.CurrentScreen == navigator.initialScreen;
-                if (shouldMinimizeApp)
+                if (shouldMinimizeApp && !clickedBefore)
                 {
-                    Application.Quit();
+                    //Set to false so that this input is not checked again. It will be checked in the coroutine function instead
+                    clickedBefore = true;
+
+                    //Activate Quit Object
+                    quitObjectImage.color = imageDefaultColor;
+                    quitObjectText.color = textDefaultColor;
+
+                    //Start quit timer
+                    StartCoroutine(quitingTimer());
+
                 }
                 else
                 {
@@ -33,38 +64,99 @@ public class InputManager : MonoBehaviour
         }
     }
 
+    private IEnumerator quitingTimer()
+    {
+        //Wait for a frame so that Input.GetKeyDown is no longer true
+        yield return null;
+
+        //3 seconds timer
+        const float timerTime = 1f;
+        float counter = 0;
+
+        while (counter < timerTime)
+        {
+            //Increment counter while it is < timer time(3)
+            counter += Time.deltaTime;
+
+            //Check if Input is pressed again while timer is running then quit/exit if is
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                //Debug.Log("Back Button pressed for the second time. EXITING.....");
+                Quit();
+            }
+
+            //Wait for a frame so that Unity does not freeze
+            yield return null;
+        }
+
+        //Timer has finished and NO QUIT(NO second press) so deactivate
+        StartCoroutine(QuitFader());
+        //Reset clickedBefore so that Input can be checked again in the Update function
+        clickedBefore = false;
+    }
+
+    private IEnumerator QuitFader()
+    {
+        currentImageCollor = quitObjectImage.color;
+        currentTextCollor = quitObjectText.color;
+        float alpha = currentImageCollor.a;
+        while (currentTextCollor.a >= 0)
+        {
+            alpha -= Time.deltaTime * 2;
+            currentImageCollor.a = alpha;
+            currentTextCollor.a = alpha;
+            quitObjectImage.color = currentImageCollor;
+            quitObjectText.color = currentTextCollor;
+            yield return null;
+        }
+        currentImageCollor.a = 0;
+        currentTextCollor.a = 0;
+        quitObjectImage.color = currentImageCollor;
+        quitObjectText.color = currentTextCollor;
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    //Application.Quit();
+    System.Diagnostics.Process.GetCurrentProcess().Kill();
+#endif
+    }
+
     //inactive original code
     #region OriginalCode
-        // // modal dialogs need to set themselves as CurrentlyOpenClosable when they're shown and unset themselves when they're hidden
-        // // it's a good idea to do this right after easyTween.OpenCloseObjectAnimation
-        // public static IClosable CurrentlyOpenClosable { get; set; }
+    // // modal dialogs need to set themselves as CurrentlyOpenClosable when they're shown and unset themselves when they're hidden
+    // // it's a good idea to do this right after easyTween.OpenCloseObjectAnimation
+    // public static IClosable CurrentlyOpenClosable { get; set; }
 
-        // [SerializeField]
-        // private SideMenu sideMenu = null;
+    // [SerializeField]
+    // private SideMenu sideMenu = null;
 
-        // void Update()
-        // {
-        //     // handle Android back key
-        //     if (Input.GetKeyDown(KeyCode.Escape))
-        //     {
-        //         if (CurrentlyOpenClosable != null)
-        //         {
-        //             CurrentlyOpenClosable.Close();
-        //         }
-        //         else
-        //         {
-        //             Navigator navigator = sideMenu.CurrentlyActiveNavigator;
-        //             bool shouldMinimizeApp = navigator.CurrentScreen == navigator.initialScreen;
-        //             if (shouldMinimizeApp)
-        //             {
-        //                 Application.Quit();
-        //             }
-        //             else
-        //             {
-        //                 sideMenu.CurrentlyActiveNavigator.GoBack();
-        //             }
-        //         }
-        //     }
-        // }
+    // void Update()
+    // {
+    //     // handle Android back key
+    //     if (Input.GetKeyDown(KeyCode.Escape))
+    //     {
+    //         if (CurrentlyOpenClosable != null)
+    //         {
+    //             CurrentlyOpenClosable.Close();
+    //         }
+    //         else
+    //         {
+    //             Navigator navigator = sideMenu.CurrentlyActiveNavigator;
+    //             bool shouldMinimizeApp = navigator.CurrentScreen == navigator.initialScreen;
+    //             if (shouldMinimizeApp)
+    //             {
+    //                 Application.Quit();
+    //             }
+    //             else
+    //             {
+    //                 sideMenu.CurrentlyActiveNavigator.GoBack();
+    //             }
+    //         }
+    //     }
+    // }
     #endregion
 }
