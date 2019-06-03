257342642e0029514b6ae8efb79e5e21443aa374
RalucaAvangarde <raluca.metes@avangarde-software.com>
2019-06-03 15:05:00 +0300

    send email to client emal adress

--- Assets/Scripts/UI/ClientsCanvas/ClientsEditScreen.cs
+++ Assets/Scripts/UI/ClientsCanvas/ClientsEditScreen.cs
@@ -12,7 +12,8 @@ public class ClientsEditScreen : MonoBehaviour
     [SerializeField]
     private InputField clientEmail;
     private IClient currentClient;
-
+    [SerializeField]
+    private Text textError;
     public IClient GetCurrentClient()
     {
         return currentClient;
@@ -25,10 +26,26 @@ public class ClientsEditScreen : MonoBehaviour
     }
     public void SetClientsFieldsText()
     {
-        clientName.text =  currentClient.Name;
+        clientName.text = currentClient.Name;
         clientPhone.text = currentClient.Number;
         clientAdress.text = currentClient.Adress;
         clientEmail.text = currentClient.Email;
+    }
+    public void ValidAdress()
+    {
+        if (RegexUtilities.IsValidEmail(clientEmail.text.ToString()))
+        {
+            Debug.Log("Email correct");
+            Debug.Log(clientEmail.text);
+        }
+
+        else
+
+        {
+           
+            Debug.Log("Invalid adress");
+            textError.text = "Please insert a valid e-mail!";
+        }
     }
 
 }
