2e0bce1d5d0881bcb5d05dfa053819b9c7f87e90
BogdanClinciu <48719928+BogdanClinciu@users.noreply.github.com>
2019-06-03 17:24:37 +0300

    Added modal calendar page object to handle month loading, added slide functionality to calendar

--- Assets/Scripts/UI/_NewModalCalendar/ModalCalendarNew.cs
+++ Assets/Scripts/UI/_NewModalCalendar/ModalCalendarNew.cs
@@ -17,17 +17,25 @@ public class ModalCalendarNew : MonoBehaviour, IClosable
     private Text selectionDayCountText = null;
     [SerializeField]
     private Button confirmButton = null;
+    [SerializeField]
+    private float slideDuration = 0.1f;
+    [SerializeField]
+    private AnimationCurve slideCurve;
 
-    [Space]
     [SerializeField]
-    private Transform rowParent = null;
+    private RectTransform slideCenter;
     [SerializeField]
-    private GameObject rowPrefab = null;
+    private RectTransform slideLeft;
     [SerializeField]
-    private GameObject dayObjectPrefab = null;
+    private RectTransform slideRight;
 
-    [Space]
+    [SerializeField]
+    private ModalMonthPage currentPage;
+    [SerializeField]
+    private ModalMonthPage cachePage;
+
     #region Colors
+    [Space]
     [SerializeField]
     private Color availableColor;
     [SerializeField]
@@ -40,60 +48,85 @@ public class ModalCalendarNew : MonoBehaviour, IClosable
     private Color pastColor;
     #endregion
 
+    #region Public readonly properties
+    public List<IReservation> RoomReservationList => roomReservationList;
+    public IReservation CurrentReservation => currentReservation;
+    public DateTime SelectedStart => selectedStart;
+    public DateTime SelectedEnd => selectedEnd;
+    public bool ShowSelection => showSelection;
+
+    public Color AvailableColor => availableColor;
+    public Color SelectedColor => selectedColor;
+    public Color UnavailableColor => unavailableColor;
+    public Color CurrentColor => currentColor;
+    public Color PastColor => pastColor;
+    #endregion
+
     private Action<DateTime, DateTime> DoneCallback;
     private DateTime focusDateTime = DateTime.Today;
     private IReservation currentReservation;
     private List<IReservation> roomReservationList = new List<IReservation>();
-    private List<ModalDayObject> dayObjects = new List<ModalDayObject>();
     private bool isSelectingEnd = false;
     private bool showSelection = false;
     private bool allowSigleDate = false;
+    private bool isSliding = false;
     private DateTime selectedStart;
     private DateTime selectedEnd;
 
+
     private void Start()
     {
-        CreateDayItems();
+        currentPage.CreateDayItems();
+        currentPage.UpdatePage(DateTime.Today.Date);
+        cachePage.CreateDayItems();
+        cachePage.UpdatePage(DateTime.Today.Date);
     }
 
-    ///<summary>
-    ///Opens the calendar in selection mode focused on the given datetime.
-    ///<para>Done callback returns either the selected datetime and the day after if only one date is selected, or the selected start and end date</para>
-    ///</summary>
-    internal void OpenCallendar(DateTime focusedDay, Action<DateTime, DateTime> doneCallback)
-    {
-        focusDateTime = focusedDay;
-        DoneCallback = doneCallback;
-        allowSigleDate = true;
-        selectionDayCountText.text = string.Empty;
-        Show(focusDateTime, null, null, doneCallback);
-    }
+    #region OpenCalendarFunctions
+        ///<summary>
+        ///Opens the calendar in selection mode focused on the given datetime.
+        ///<para>Done callback returns either the selected datetime and the day after if only one date is selected, or the selected start and end date</para>
+        ///</summary>
+        internal void OpenCallendar(DateTime focusedDay, Action<DateTime, DateTime> doneCallback)
+        {
+            focusDateTime = focusedDay;
+            DoneCallback = doneCallback;
+            allowSigleDate = true;
+            selectionDayCountText.text = string.Empty;
+            Show(focusDateTime, null, null, doneCallback);
+        }
 
-    ///<summary>
-    ///Opens the calendar in reservation edit mode for the given IReservation, and room reservations list.
-    ///<para>Done callback returns the selected start and end date</para>
-    ///</summary>
-    internal void OpenCallendar(IReservation r, List<IReservation> roomReservations, Action<DateTime, DateTime> doneCallback)
-    {
-        focusDateTime = (r != null) ? r.Period.Start : DateTime.Today;
-        allowSigleDate = false;
-        UpdateDayCountText();
-        Show(focusDateTime, r, roomReservations, doneCallback);
-    }
+        ///<summary>
+        ///Opens the calendar in reservation edit mode for the given IReservation, and room reservations list.
+        ///<para>Done callback returns the selected start and end date</para>
+        ///</summary>
+        internal void OpenCallendar(IReservation r, List<IReservation> roomReservations, Action<DateTime, DateTime> doneCallback)
+        {
+            focusDateTime = (r != null) ? r.Period.Start : DateTime.Today;
+            allowSigleDate = false;
+            UpdateDayCountText();
+            Show(focusDateTime, r, roomReservations, doneCallback);
+        }
+    #endregion
 
     public void ShowPreviousMonth()
     {
-        if (focusDateTime.Date > DateTime.Today.Date)
+        if (focusDateTime.Date > DateTime.Today.Date && !isSliding)
         {
             focusDateTime = focusDateTime.AddMonths(-1);
-            UpdateCalendar(focusDateTime);
+            //UpdateCalendar(focusDateTime);
+            StartCoroutine(Swipe(true));
         }
     }
 
     public void ShowNextMonth()
     {
-        focusDateTime = focusDateTime.AddMonths(1);
-        UpdateCalendar(focusDateTime);
+        if(!isSliding)
+        {
+            focusDateTime = focusDateTime.AddMonths(1);
+            // UpdateCalendar(focusDateTime);
+            StartCoroutine(Swipe(false));
+        }
     }
 
     public void Close()
@@ -106,22 +139,25 @@ public class ModalCalendarNew : MonoBehaviour, IClosable
         CloseModalCalendar(false);
     }
 
-    private void CreateDayItems()
+    public int GetDaysVisibleFromPreviousMonth(DayOfWeek day)
     {
-        for (int r = 0; r < 6; r++)//rows
+        switch (day)
         {
-            Transform row = Instantiate(rowPrefab, rowParent).transform;
-            for (int i = 0; i < 7; i++) //days
-            {
-                ModalDayObject m = Instantiate(dayObjectPrefab,row).GetComponent<ModalDayObject>();
-                m.SetListener(HandleClickAction);
-                dayObjects.Add(m);
-            }
+            case DayOfWeek.Monday: return 0;
+            case DayOfWeek.Tuesday: return 1;
+            case DayOfWeek.Wednesday: return 2;
+            case DayOfWeek.Thursday: return 3;
+            case DayOfWeek.Friday: return 4;
+            case DayOfWeek.Saturday: return 5;
+            case DayOfWeek.Sunday: return 6;
         }
+
+        return 0;
     }
 
     private void Show(DateTime initialDateTime, IReservation reservation, List<IReservation> reservationList, Action<DateTime, DateTime> doneCallback)
     {
+        monthName.text = Constants.MonthNamesDict[focusDateTime.Month] + " " + focusDateTime.Year;
         focusDateTime = initialDateTime;
         currentReservation = reservation;
         roomReservationList = reservationList;
@@ -140,10 +176,10 @@ public class ModalCalendarNew : MonoBehaviour, IClosable
             confirmButton.interactable = false;
         }
 
-        UpdateCalendar(focusDateTime);
+        currentPage.UpdatePage(focusDateTime);
     }
 
-    private void HandleClickAction(ModalDayObject dayObj)
+    public void HandleClickAction(ModalDayObject dayObj)
     {
         if(!dayObj.IsReserved && !dayObj.IsStart && !isSelectingEnd)
         {
@@ -151,7 +187,7 @@ public class ModalCalendarNew : MonoBehaviour, IClosable
             selectedEnd = dayObj.ObjDate.Date;
             isSelectingEnd = true;
             showSelection = true;
-            UpdateCalendar(focusDateTime);
+            currentPage.UpdatePage(focusDateTime);
             selectionText.text = selectedStart.ToString(Constants.DateTimePrintFormat);
             confirmButton.interactable = allowSigleDate;
         }
@@ -177,14 +213,14 @@ public class ModalCalendarNew : MonoBehaviour, IClosable
                 selectionText.text = string.Empty;
                 confirmButton.interactable = false;
             }
-            UpdateCalendar(focusDateTime);
+            currentPage.UpdatePage(focusDateTime);
             isSelectingEnd = false;
         }
         else
         {
             showSelection = false;
             isSelectingEnd = false;
-            UpdateCalendar(focusDateTime);
+            currentPage.UpdatePage(focusDateTime);
             selectionText.text = string.Empty;
             confirmButton.interactable = false;
         }
@@ -212,129 +248,6 @@ public class ModalCalendarNew : MonoBehaviour, IClosable
         roomReservationList = new List<IReservation>();
     }
 
-    private void UpdateCalendar(DateTime selectedDateTime)
-    {
-        DateTime firstDayOfMonthInSelectedDate = new DateTime(selectedDateTime.Year, selectedDateTime.Month, 1, 0, 0, 0, DateTimeKind.Local);
-
-
-        SetDayItemsForPreviousMonth(selectedDateTime, firstDayOfMonthInSelectedDate);
-
-        SetDayItemsForCurrentMonth(selectedDateTime, firstDayOfMonthInSelectedDate);
-
-        SetDayItemsForNextMonth(selectedDateTime, firstDayOfMonthInSelectedDate);
-
-        UpdateCurentReservationPeriod();
-
-        if(showSelection)
-        {
-            UpdatePeriodSelection();
-        }
-    }
-
-    private void SetDayItemsForNextMonth(DateTime selectedDateTime, DateTime firstDayOfMonthInSelectedDate)
-    {
-        DateTime nextMonthDateTime = selectedDateTime.AddMonths(1);
-        int daysInSelectedMonth = DateTime.DaysInMonth(selectedDateTime.Year, selectedDateTime.Month);
-        int daysVisibleFromPreviousMonth = GetDaysVisibleFromPreviousMonth(firstDayOfMonthInSelectedDate.DayOfWeek);
-        int firstDayFromNextMonthIndex = daysInSelectedMonth + daysVisibleFromPreviousMonth;
-        int dayVisibleFromNextMonth = 1;
-
-        for (int d = firstDayFromNextMonthIndex; d < dayObjects.Count; d++)
-        {
-            nextMonthDateTime = new DateTime(nextMonthDateTime.Year, nextMonthDateTime.Month, dayVisibleFromNextMonth, 0, 0, 0, DateTimeKind.Local);
-            SetReservationType(dayObjects[d], nextMonthDateTime);
-            dayVisibleFromNextMonth++;
-        }
-    }
-
-    private void SetDayItemsForCurrentMonth(DateTime selectedDateTime, DateTime firstDayOfMonthInSelectedDate)
-    {
-        monthName.text = Constants.MonthNamesDict[selectedDateTime.Month] + " " + selectedDateTime.Year;
-        int daysInMonth = DateTime.DaysInMonth(selectedDateTime.Year, selectedDateTime.Month);
-        int daysVisibleFromPreviousMonth = GetDaysVisibleFromPreviousMonth(firstDayOfMonthInSelectedDate.DayOfWeek);
-        int daysVisibleInCurrentMonth = daysInMonth + daysVisibleFromPreviousMonth;
-        int dayVisibleFromSelectedMonth = 1;
-
-        for (int i = daysVisibleFromPreviousMonth; i < daysVisibleInCurrentMonth; i++)
-        {
-            selectedDateTime = new DateTime(selectedDateTime.Year, selectedDateTime.Month, dayVisibleFromSelectedMonth, 0, 0, 0, DateTimeKind.Local);
-
-            SetReservationType(dayObjects[i], selectedDateTime);
-            dayVisibleFromSelectedMonth++;
-        }
-    }
-
-    private void SetDayItemsForPreviousMonth(DateTime selectedDateTime, DateTime firstDayOfMonthInSelectedDate)
-    {
-        DateTime previousMonthDateTime = selectedDateTime.AddMonths(-1);
-        int dayVisibleFromPreviousMonth = DateTime.DaysInMonth(previousMonthDateTime.Year, previousMonthDateTime.Month);
-
-        for (int p = GetDaysVisibleFromPreviousMonth(firstDayOfMonthInSelectedDate.DayOfWeek) - 1; p >= 0; --p)
-        {
-            previousMonthDateTime = new DateTime(previousMonthDateTime.Year, previousMonthDateTime.Month, dayVisibleFromPreviousMonth, 0, 0, 0, DateTimeKind.Local);
-            SetReservationType(dayObjects[p], previousMonthDateTime);
-            dayVisibleFromPreviousMonth--;
-        }
-    }
-
-    private int GetDaysVisibleFromPreviousMonth(DayOfWeek day)
-    {
-        switch (day)
-        {
-            case DayOfWeek.Monday: return 0;
-            case DayOfWeek.Tuesday: return 1;
-            case DayOfWeek.Wednesday: return 2;
-            case DayOfWeek.Thursday: return 3;
-            case DayOfWeek.Friday: return 4;
-            case DayOfWeek.Saturday: return 5;
-            case DayOfWeek.Sunday: return 6;
-        }
-
-        return 0;
-    }
-
-    private void SetReservationType(ModalDayObject dayObj, DateTime dateTime)
-    {
-        dayObj.UpdateDayObject(dateTime);
-
-        string currentReservationID = (currentReservation != null) ? currentReservation.ID : Constants.defaultCustomerName;
-
-        dayObj.UpdateDayColors(availableColor, availableColor, availableColor);
-        dayObj.IsReserved = false;
-        dayObj.IsEnd = false;
-        dayObj.IsStart = false;
-
-        if(dateTime.Date < DateTime.Today.Date)
-        {
-            dayObj.UpdateDayColors(pastColor, pastColor, pastColor);
-            dayObj.IsReserved = true;
-            return;
-        }
-
-        if(roomReservationList == null || roomReservationList.Count == 0)
-        {
-            return;
-        }
-
-        if(roomReservationList.Any(r => r.Period.Start.Date == dateTime.Date))
-        {
-            dayObj.UpdateDayColors(null, unavailableColor, null);
-            dayObj.IsStart = true;
-        }
-
-        if(roomReservationList.Any(r => r.Period.End.Date == dateTime.Date))
-        {
-            dayObj.UpdateDayColors(null, null, unavailableColor);
-            dayObj.IsEnd = true;
-        }
-
-        if(roomReservationList.Any(r => dateTime.Date < r.Period.End.Date && dateTime.Date > r.Period.Start.Date))
-        {
-            dayObj.UpdateDayColors(unavailableColor, unavailableColor, unavailableColor);
-            dayObj.IsReserved = true;
-        }
-    }
-
     private bool OverlapsOtherReservation(DateTime start, DateTime end)
     {
         if(roomReservationList == null || roomReservationList.Count == 0)
@@ -350,77 +263,41 @@ public class ModalCalendarNew : MonoBehaviour, IClosable
             ));
     }
 
-    private void UpdatePeriodSelection()
-    {
-        if(selectedStart.Date == selectedEnd.Date)
-        {
-            if(dayObjects.Any(d => d.ObjDate.Date == selectedStart.Date))
-            {
-                dayObjects.Find(d => d.ObjDate.Date == selectedStart.Date).UpdateDayColors(null,selectedColor, null);
-            }
-            return;
-        }
-
-        for (int d = 0; d < dayObjects.Count; d++)
-        {
-            if(dayObjects[d].ObjDate.Date > selectedStart.Date && dayObjects[d].ObjDate.Date < selectedEnd.Date)
-            {
-                dayObjects[d].UpdateDayColors(selectedColor, selectedColor, selectedColor);
-            }
-            else
-            {
-                if(dayObjects[d].ObjDate == selectedStart)
-                {
-                    dayObjects[d].UpdateDayColors(null,selectedColor, null);
-                }
-
-                if(dayObjects[d].ObjDate == selectedEnd)
-                {
-                    dayObjects[d].UpdateDayColors(null, null, selectedColor);
-                }
-            }
-
-        }
-    }
-
-    private void UpdateCurentReservationPeriod()
-    {
-        if(currentReservation != null)
-        {
-            for (int d = 0; d < dayObjects.Count; d++)
-            {
-
-                if(dayObjects[d].ObjDate.Date < DateTime.Today.Date)
-                {
-                    dayObjects[d].UpdateDayColors(pastColor, pastColor, pastColor);
-                    dayObjects[d].IsReserved = true;
-                }
-                else
-                {
-                    if(dayObjects[d].ObjDate.Date > currentReservation.Period.Start.Date && dayObjects[d].ObjDate.Date < currentReservation.Period.End.Date)
-                    {
-                        dayObjects[d].UpdateDayColors(currentColor, currentColor, currentColor);
-                    }
-                    else
-                    {
-                        if(dayObjects[d].ObjDate.Date == currentReservation.Period.Start.Date)
-                        {
-                            dayObjects[d].UpdateDayColors(null,currentColor, null);
-                        }
-
-                        if(dayObjects[d].ObjDate.Date == currentReservation.Period.End.Date)
-                        {
-                            dayObjects[d].UpdateDayColors(null, null, currentColor);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     private void UpdateDayCountText()
     {
         int days = (int)(selectedEnd - selectedStart).TotalDays;
         selectionDayCountText.text = String.Format("{0} {1} {2}", Constants.DAY_COUNT_PREF, days, (days == 1) ? Constants.DAY_COUNT_SUFF_SN : Constants.DAY_COUNT_SUFF_PL);
+    }
+
+    private IEnumerator Swipe(bool isLeft)
+    {
+        isSliding = true;
+
+        monthName.text = Constants.MonthNamesDict[focusDateTime.Month] + " " + focusDateTime.Year;
+
+        cachePage.Rect.position = (isLeft) ? slideLeft.position : slideRight.position;
+        cachePage.UpdatePage(focusDateTime);
+
+        Vector3 startCache = cachePage.Rect.position;
+        Vector3 endCache = slideCenter.position;
+
+        Vector3 startCurrent = currentPage.Rect.position;
+        Vector3 endCurrent = (!isLeft) ? slideLeft.position : slideRight.position;
+
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime/slideDuration)
+        {
+            cachePage.Rect.position = Vector3.Lerp(startCache, endCache, slideCurve.Evaluate(t));
+            currentPage.Rect.position = Vector3.Lerp(startCurrent, endCurrent, slideCurve.Evaluate(t));
+            yield return null;
+        }
+
+        cachePage.Rect.position = endCache;
+        currentPage.Rect.position = endCurrent;
+
+        ModalMonthPage cache = currentPage;
+        currentPage = cachePage;
+        cachePage = cache;
+
+        isSliding = false;
     }
 }
