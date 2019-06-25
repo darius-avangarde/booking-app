ac778d2a88c085c0830407493bf6514e1d2317d7
BogdanClinciu <48719928+BogdanClinciu@users.noreply.github.com>
2019-06-25 17:05:41 +0300

    Reverted to old image blurring.

--- Assets/Scripts/Data/ImageDataManager.cs
+++ Assets/Scripts/Data/ImageDataManager.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Networking;
 
 public class ImageDataManager
 {
