57dfa6198d1007a7015a858dfd631c43a1f686b4
BogdanClinciu <48719928+BogdanClinciu@users.noreply.github.com>
2019-07-31 14:02:37 +0300

    Added room pool start initialization with a number of items.

--- Assets/Scripts/UI_New/NewReservationEditScreen/RoomToggleDropdown/RoomToggleDropdown.cs
+++ Assets/Scripts/UI_New/NewReservationEditScreen/RoomToggleDropdown/RoomToggleDropdown.cs
@@ -32,6 +32,15 @@ public class RoomToggleDropdown : MonoBehaviour, IClosable
     private List<RoomToggleObject> pool = new List<RoomToggleObject>();
     private List<IRoom> currentSelection = new List<IRoom>();
 
+    private void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            CreateNewRoomObject();
+            pool[i].DisableObject();
+        }
+    }
+
     public void InitializeRoomDropdown(UnityAction<List<IRoom>> selectionCallback, List<IRoom> allRooms, List<IRoom> selectedRooms = null)
     {
         callback = selectionCallback;
