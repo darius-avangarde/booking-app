8702bfe4f9f9671151e156121b354e46f2e1d5df
BogdanClinciu <48719928+BogdanClinciu@users.noreply.github.com>
2019-08-13 17:29:42 +0300

    Expanded prototype. Improved clamping and smoothness.

--- Assets/Scripts/UI_New/MainMenuCalendar/ReservationObjectManager/ReservationObjectManager.cs
+++ Assets/Scripts/UI_New/MainMenuCalendar/ReservationObjectManager/ReservationObjectManager.cs
@@ -61,27 +61,59 @@ public class ReservationObjectManager : MonoBehaviour
         if(resObj.ObjRectTransform.rect.width < resObj.ObjTextTransform.rect.width * 3)
             return;
 
-        Vector2 objBoundsX;
-        objBoundsX.x = resObj.ObjRectTransform.TransformPoint(resObj.ObjRectTransform.rect.min).x;
-        objBoundsX.y = resObj.ObjRectTransform.TransformPoint(resObj.ObjRectTransform.rect.max).x;
+        // Vector2 objBoundsX;
+        // objBoundsX.x = resObj.ObjRectTransform.TransformPoint(resObj.ObjRectTransform.rect.min).x;
+        // objBoundsX.y = resObj.ObjRectTransform.TransformPoint(resObj.ObjRectTransform.rect.max).x;
 
-        float clampMin = Mathf.Max(objBoundsX.x, vpBounds.x) + resObj.ObjTextTransform.rect.width/2 + 120;
-        float clampMax = Mathf.Min(objBoundsX.y, vpBounds.y) - resObj.ObjTextTransform.rect.width/2;
+        Vector2 localObjBoundsX;
+        localObjBoundsX.x = -resObj.ObjRectTransform.rect.width/2;
+        localObjBoundsX.y = resObj.ObjRectTransform.rect.width/2;
 
-        if(resObj.ObjTextTransform.position.x < clampMax - 60 && resObj.ObjTextTransform.position.x > clampMin + 60)
-        {
-            return;
-        }
-        else
-        {
-            float targetCenterX = reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.center).x;
-            // Vector3 finalPosition = new Vector3(Mathf.Lerp(resObj.ObjTextTransform.position.x, Mathf.Clamp(targetCenterX, clampMin, clampMax), Time.deltaTime * 10f) , resObj.ObjTextTransform.position.y, resObj.ObjTextTransform.position.z);
-            // resObj.ObjTextTransform.localPosition = resObj.ObjTextTransform.InverseTransformPoint(finalPosition);
-            resObj.ObjTextTransform.position = new Vector3(Mathf.Lerp(resObj.ObjTextTransform.position.x, Mathf.Clamp(targetCenterX, clampMin, clampMax), Time.deltaTime * 10f) , resObj.ObjTextTransform.position.y, resObj.ObjTextTransform.position.z);;
-        }
+        Vector2 localVpBounds;
+        localVpBounds.x = resObj.ObjTextTransform.InverseTransformPoint(reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.min)).x;
+        localVpBounds.y = resObj.ObjTextTransform.InverseTransformPoint(reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.max)).x;
+
+
+        // float clampMin = Mathf.Max(objBoundsX.x, vpBounds.x) + resObj.ObjTextTransform.rect.width/2 + 120;
+        // float clampMax = Mathf.Min(objBoundsX.y, vpBounds.y) - resObj.ObjTextTransform.rect.width/2;
+
+        // float localClampMin = Mathf.Max(localObjBoundsX.x, localVpBounds.x) + resObj.ObjTextTransform.rect.width/2 + 120;
+        // float localClampMax = Mathf.Min(localObjBoundsX.y, localVpBounds.y) - resObj.ObjTextTransform.rect.width/2;
+
+        float localClampMin = - resObj.ObjRectTransform.rect.width/2 + resObj.ObjTextTransform.rect.width/2 + 10; //local clams are GOOD
+        float localClampMax =  resObj.ObjRectTransform.rect.width/2  - resObj.ObjTextTransform.rect.width/2 - 10;
+
+            //TODO: clamps are GOOD, find better local representation of viewport center
+            float targetCenterX = resObj.ObjTextTransform.InverseTransformPoint(reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.center)).x;
+            resObj.ObjTextTransform.localPosition = new Vector3(Mathf.Lerp(resObj.ObjTextTransform.localPosition.x, Mathf.Clamp(targetCenterX, localClampMin, localClampMax), Time.deltaTime * 10f) , resObj.ObjTextTransform.localPosition.y, resObj.ObjTextTransform.localPosition.z);
+
+        Debug.DrawLine(Vector2.zero, reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.center), Color.green);
+        Debug.DrawLine(reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.center) ,reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.center) + resObj.ObjTextTransform.InverseTransformPoint(reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.center)), Color.red);
+
+        // if(resObj.ObjTextTransform.position.x < clampMax - 60 && resObj.ObjTextTransform.position.x > clampMin + 60)
+        // {
+        //     return;
+        // }
+        // else
+        // {
+        //     float targetCenterX = reservationsScrolrect.viewport.TransformPoint(reservationsScrolrect.viewport.rect.center).x;
+        //     resObj.ObjTextTransform.position = new Vector3(Mathf.Lerp(resObj.ObjTextTransform.position.x, Mathf.Clamp(targetCenterX, clampMin, clampMax), Time.deltaTime * 10f) , resObj.ObjTextTransform.position.y, resObj.ObjTextTransform.position.z);;
+        // }
 
     }
 
+
+    public static Vector3 TransformPointUnscaled(Transform trs, Vector3 pos)
+    {
+        var localToWorldMatrix = Matrix4x4.TRS(trs.position, trs.rotation, Vector3.one);
+        return localToWorldMatrix.MultiplyPoint3x4(pos);
+    }
+
+    public static Vector3 InverseTransformPointUnscaled(Transform trs, Vector3 pos)
+    {
+        var worldToLocalMatrix = Matrix4x4.TRS(trs.position, trs.rotation, Vector3.one).inverse;
+        return worldToLocalMatrix.MultiplyPoint3x4(pos);
+    }
 
 
 
